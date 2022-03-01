3032cd6f9e142db8cbe851360391952c9c5e03fd
deflationz <95709937+deflationz@users.noreply.github.com>
2022-03-01 14:27:49 -0500

    Added file open window via tkinter

--- ImagetoSketch.py
+++ ImagetoSketch.py
@@ -1,11 +1,24 @@
 try:
+    from tkinter import Tk
+except ModuleNotFoundError:
+    import os
+    os.sys('pip install tkinter')
+    from tkinter import Tk
+try:
+    from tkinter.filedialog import askopenfilename
+except ModuleNotFoundError:
+    import os
+    os.sys('pip install tkinter')
+    from tkinter.filedialog import askopenfilename
+try:
     import cv2
 except ModuleNotFoundError:
     import os
     os.sys('pip install opencv-python')
     import cv2
-print('''Enter your image path, ex: "C:\Users\Admin\Downloads\Image to Sketch Project\image1.jpg"''')
-file = input()
+Tk().withdraw()
+filename = askopenfilename()
+file = filename
 img = cv2.imread(file)
 grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 inv = cv2.bitwise_not(grey)
